7c69ff54b7b0d840600b451933cc1cdcd0701ff4
unknown <branden.phan@gmail.com>
2021-06-16 21:03:36 -0400

    Made changes to manifest.json removing logos from create-react-app

--- src/index.js
+++ src/index.js
@@ -3,3 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*
+ * Author: Branden Phan
+ * Email: branden.phan@gmail.com
+ * Date: June 16, 2021
+ */
